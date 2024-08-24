d715e7cf76b9a8a899874e94a60abbc01ee93e50
ElKaloria <maks.dektyarev@yandex.ru>
2024-08-24 15:51:11 +0700

    fix created_at column

--- src/note_app/models.py
+++ src/note_app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, DATETIME
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP
 
 metadata = MetaData()
 
@@ -10,5 +10,5 @@ notes = Table(
     Column("id", Integer, primary_key=True),
     Column("title", String),
     Column("text", String),
-    Column("created_at", DATETIME, default=datetime.utcnow()),
+    Column("created_at", TIMESTAMP, default=datetime.utcnow()),
 )
