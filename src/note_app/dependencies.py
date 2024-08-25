5a27a465ec18a62835a407ce8205f295bcc1bb13
ElKaloria <maks.dektyarev@yandex.ru>
2024-08-25 17:24:27 +0700

    add docstrings for functions and routs

--- src/note_app/dependencies.py
+++ src/note_app/dependencies.py
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import Any, Tuple
 
 import requests
 from fastapi import Depends
-from sqlalchemy import select
+from sqlalchemy import select, Row
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.note_app.schemas import CreateNote
@@ -13,7 +13,19 @@ from fastapi import Response
 
 async def valid_post_id(post_id: int,
                         response: Response,
-                        session: AsyncSession = Depends(get_async_session)):
+                        session: AsyncSession = Depends(get_async_session)) -> Row[tuple[Any]] | dict[str, str]:
+    """
+        Check if a post with the given ID exists in the database.
+
+        Args:
+            post_id (int): The ID of the post to check.
+            response (Response): The response object to set the status code on if the post is not found.
+            session (AsyncSession, optional): The database session to use. Defaults to the session returned by get_async_session.
+
+        Returns:
+           Row[tuple[Any]] | Dict[str, str]: The post data if it exists, or messeges if it doesn't.
+    """
+
     query = select(notes).where(notes.c.id == post_id)
     post = await session.execute(query)
     result = post.first()
@@ -24,7 +36,17 @@ async def valid_post_id(post_id: int,
     return result
 
 
-async def valid_text_and_title(note: CreateNote):
+async def valid_text_and_title(note: CreateNote) -> dict[str, str]:
+    """
+        Check the spelling of the note's title and text using the Yandex Speller API.
+
+        Args:
+            note (CreateNote): The note to check.
+
+        Returns:
+            Dict[str, str]: The note data with corrected spelling.
+    """
+
     spell_note_text = requests.get(f"https://speller.yandex.net/services/"
                                    f"spellservice.json/checkText?text={note.text}").json()
     spell_note_title = requests.get(f"https://speller.yandex.net/services/"
--- src/note_app/routers.py
+++ src/note_app/routers.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
-import requests
 from src.note_app.dependencies import valid_post_id, valid_text_and_title
 from src.note_app.models import notes
 from src.database import get_async_session
-from src.note_app.schemas import NoteSchema, CreateNote, Message
+from src.note_app.schemas import NoteSchema, CreateNote
 
 router = APIRouter(
     prefix="/notes",
@@ -16,6 +15,18 @@ router = APIRouter(
 @router.get("/", response_model=list[NoteSchema])
 async def get_all_notes(session: AsyncSession = Depends(get_async_session),
                         page: int = 1, limit: int = 10):
+    """
+        Get all notes.
+
+        Args:
+            session (AsyncSession): The database session.
+            page (int): The page number.
+            limit (int): The number of notes per page.
+
+        Returns:
+            List[NoteSchema]: The list of notes.
+    """
+
     offset = (page - 1) * limit
     query = select(notes).offset(offset).limit(limit)
     result = await session.execute(query)
@@ -25,6 +36,17 @@ async def get_all_notes(session: AsyncSession = Depends(get_async_session),
 @router.post("/create_note", response_model=CreateNote)
 async def create_note(note: CreateNote = Depends(valid_text_and_title),
                       session: AsyncSession = Depends(get_async_session)):
+    """
+        Create a new note.
+
+        Args:
+            note (CreateNote): The note to create.
+            session (AsyncSession): The database session.
+
+        Returns:
+            CreateNote: The created note.
+    """
+
     stmt = insert(notes).values(**note)
     await session.execute(stmt)
     await session.commit()
@@ -34,5 +56,15 @@ async def create_note(note: CreateNote = Depends(valid_text_and_title),
 @router.get("/{note_by_id}",
             response_model=NoteSchema)
 async def get_note_by_id(note_by_id: NoteSchema | dict[str, str] = Depends(valid_post_id)):
+    """
+        Get a note by ID.
+
+        Args:
+            note_by_id NoteSchema | dict[str, str]: The note ID.
+
+        Returns:
+            NoteSchema | dict[str, str]: The note or error message.
+    """
+
     return note_by_id
 
