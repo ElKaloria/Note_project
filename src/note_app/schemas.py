132b0bcf1ada5d12a5beb345775dabed500f2ad8
ElKaloria <maks.dektyarev@yandex.ru>
2024-08-25 15:01:23 +0700

    add CreateNote schema

--- src/note_app/schemas.py
+++ src/note_app/schemas.py
@@ -1,11 +1,20 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class NoteSchema(BaseModel):
     id: int
     title: str
     text: str
-    created_at: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class CreateNote(BaseModel):
+    title: str
+    text: str
 
     class Config:
         orm_mode = True
